acba8a74648f7836d538c66c7db508b758ece050
vertickt <verticktblue@gmail.com>
2025-11-15 14:51:18 +0100

    optimized imports
    Co-authored-by: pierrelasse <76223390+pierrelasse@users.noreply.github.com>

--- src/main/kotlin/io/github/vertickt/speedrun/Speedrun.kt
+++ src/main/kotlin/io/github/vertickt/speedrun/Speedrun.kt
@@ -4,7 +4,6 @@ import io.github.vertickt.speedrun.command.StartCommand
 import io.github.vertickt.speedrun.gamemanger.Round
 import io.github.vertickt.speedrun.listener.ConnectionListener
 import net.axay.kspigot.main.KSpigot
-import net.axay.kspigot.main.KSpigotMainInstance
 
 class Speedrun : KSpigot() {
 
